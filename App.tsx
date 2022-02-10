87507eecdf42f335484964125bc5cef4fcd753f3
rafael <rafael.si2018.2@gmail.com>
2022-02-10 19:57:29 -0300

    firebase notification

--- App.tsx
+++ App.tsx
@@ -6,11 +6,22 @@ import AppLoading from 'expo-app-loading';
 import { ThemeProvider } from 'styled-components/native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { useFonts, Inter_400Regular, Inter_700Bold } from '@expo-google-fonts/inter';
+import messaging from '@react-native-firebase/messaging';
 
 import { Routes } from './src/routes';
 import theme from './src/theme';
 
 export default function App() {
+  async function requestUserPermission() {
+    const authStatus = await messaging().requestPermission();
+    const enabled =
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+  
+    if (enabled) {
+      console.log('Authorization status:', authStatus);
+    }
+  }
   const [fontsLoaded] = useFonts({
     Inter_700Bold,
     Inter_400Regular
